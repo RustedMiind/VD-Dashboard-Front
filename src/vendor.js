f4fd81ee14005a9fe4bf4624560995be825cf04d
Ali Soliman <alisolimanworks@gmail.com>
2023-07-19 14:04:54 +0300

    Imported main.scss

--- src/vendor.js
+++ src/vendor.js
@@ -2,4 +2,5 @@ require("../scss/custom-bootstrap.scss");
 require("../node_modules/jquery/dist/jquery.slim.min");
 require("../node_modules/@popperjs/core/dist/umd/popper.min");
 require("../node_modules/bootstrap/dist/js/bootstrap.bundle.min");
+require("../scss/main.scss");
 require("lucide");
